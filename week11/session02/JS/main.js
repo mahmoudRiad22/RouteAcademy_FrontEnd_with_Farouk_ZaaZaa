956549669724fc6ae01e412396fd91af706d06b6
mahmoudRiad22 <mahmoudriad027@gmail.com>
2025-08-27 01:17:33 +0300

    week11 session02: 1- finised form design. 2- finished Form CR with session storage. 3-validation logic for Name and Email.

--- week11/session02/JS/main.js
+++ week11/session02/JS/main.js
@@ -66,8 +66,8 @@ const minIndex = 0;
 const maxIndex = quotesList.length - 1;
 const random = Math.floor(Math.random() * (maxIndex - minIndex + 1)) + minIndex;
 let counter = 0;
-console.log('quotesList :>> ', quotesList);
-console.log('random :>> ', random);
+console.log("quotesList :>> ", quotesList);
+console.log("random :>> ", random);
 
 function noRepeate(oldRandom) {
     let newRandom = Math.floor(Math.random() * (maxIndex - minIndex + 1)) + minIndex;
@@ -104,7 +104,6 @@ function displayQuote(id) {
 // #region Local/Session Storage
 console.clear();
 
-
 const people = [
     {name: "Emma Johnson", city: "New York"},
     {name: "Liam Smith", city: "Los Angeles"},
@@ -127,7 +126,7 @@ sessionStorage.setItem("people.json", JSON.stringify(people));
 console.log("sessionStorage :>> ", sessionStorage.getItem("people.json"));
 
 // sessionStorage.setItem('people.obj', people); // does not worrk at all its just all objects
-console.log('get key name using index  :>> ', sessionStorage.getItem('people.obj') );
+console.log("get key name using index  :>> ", sessionStorage.getItem("people.obj"));
 
 const myPpl = JSON.parse(sessionStorage.getItem("people.json"));
 console.log("people :>> ", people);
@@ -143,15 +142,15 @@ console.log("people :>> ", people);
 console.clear();
 
 const inputArray = [];
-function readInput() {
+function TernaryReadInput() {
     const input0 = document.getElementById("userInput0");
     console.log("input0 :>> ", input0.value);
 
-    inputArray.push(input0.value? input0.value : 'dummy data');
-    console.log('inputArray.length :>> ', inputArray.length);
+    inputArray.push(input0.value ? input0.value : "dummy data");
+    console.log("inputArray.length :>> ", inputArray.length);
 
-    inputArray.forEach(name => {
-        console.log('name :>> ', name);
+    inputArray.forEach((name) => {
+        console.log("name :>> ", name);
     });
 }
 
@@ -161,13 +160,141 @@ function readInput() {
 
 /********************************************/
 /********************************************/
-// #region Input Form
+// #region Input Form and Validation
 console.clear();
 
+const Name = document.getElementById("userName");
+const email = document.getElementById("userEmail");
+const password = document.getElementById("userPassword");
+const rePassword = document.getElementById("userRePassword");
+const phone = document.getElementById("userPhone");
+const img = document.getElementById("userImage");
+const msg = document.getElementById("userMsg");
 
+const submitBtn = document.getElementById("submitBtn");
 
+const allUsers = [];
+const IMAGES_PATH = "./images";
+const DEFAULT_IMG_SRC = "./images/images3/imgi_26_09.jpg";
+
+function formReadInput() {
+    const user = {
+        name: Name.value ? Name.value : "Unknown",
+        email: email.value ? email.value : "Unknown",
+        password: password.value ? password.value : "Unknown1",
+        rePassword: rePassword.value ? rePassword.value : "Unknown2",
+        phone: phone.value ? phone.value : 0,
+        img: img.value ? IMAGES_PATH + img.value.name : DEFAULT_IMG_SRC,
+        msg: msg.value ? msg.value : "Unknown",
+    };
+
+    if (
+        user.name != "Unknown" &&
+        user.email != "Unknown" &&
+        user.password != "Unknown1" &&
+        user.phone != 0 &&
+        user.img != "Unknown" &&
+        user.msg != "Unknown"
+    ) {
+        console.log("All data are recieved");
+        console.log("user :>> ", user);
+        if (validateAll) {
+            console.log("allUsers.length :>> ", allUsers.push(user));
+            sessionStorage.setItem("allUsers", JSON.stringify(allUsers));
+            console.log("User data is saved in sorage :>> ", true);
+            formClearInput();
+        }
+    } else {
+        console.warn("Not all data are given");
+        formClearInput();
+    }
+}
+
+function validateAll() {
+    return validateName() * validateEmail();
+}
+function validateName() {
+    // const name = Name.value;
+    // console.log('name :>> ', name);
+    const nameRegExp = /^[A-Z]\w{2,20}$/i; //any word with min 3chars and max 20chars
+
+    if (nameRegExp.test(Name.value)) {
+        submitBtn.removeAttribute("disabled", "");
+        Name.classList.add("is-valid");
+        Name.classList.remove("is-invalid");
+        removeAlert("userNameAlert");
+        return 1;
+    } else {
+        submitBtn.setAttribute("disabled", "");
+        Name.classList.remove("is-valid");
+        Name.classList.add("is-invalid");
+        displayALert("userNameAlert", "Invalid Name", "danger");
+        return 0;
+    }
+}
+
+function validateEmail() {
+    const emailRegExp = /^\w+@[a-z]+\.[a-z]{2,}$/i;
+
+    if (emailRegExp.test(email.value)) {
+        email.classList.add("is-valid");
+        email.classList.remove("is-invalid");
+        removeAlert("userEmailAlert");
+        submitBtn.removeAttribute("disabled", "");
+    } else {
+        email.classList.add("is-invalid");
+        email.classList.remove("is-valid");
+        displayALert("userEmailAlert", "Invalid Email", "danger");
+        submitBtn.setAttribute("disabled", "");
+    }
+    return;
+}
+// TODO:
+// 1- validate password
+// 2- repassword matches
+// 3- validate phone number
+
+function displayALert(target, message, type) {
+    const alertPlaceholder = document.getElementById(target);
+
+    // const wrapper = document.createElement("div");
+    alertPlaceholder.innerHTML = `<div class="alert alert-${type} alert-dismissible mt-1" role="alert">
+            <div>${message}</div>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>`;
+    // alertPlaceholder.append(wrapper);
+}
+function removeAlert(target) {
+    const alertPlaceholder = document.getElementById(target);
+    alertPlaceholder.innerHTML = "";
+}
+
+function formClearInput() {
+    Name.value = "";
+    email.value = "";
+    password.value = "";
+    rePassword.value = "";
+    phone.value = 0;
+    img.value = "";
+    msg.value = "";
+}
+
+// validateWithKeyWord("number", Number("1231341312"));
+
+function validateWithKeyWord(key, input0) {
+    const regExp = /^[0-9]+$/;
+    // const nameRegExp = new RegExp();
+
+    switch (key) {
+        case "number":
+            console.log("regExp :>> ", regExp.test(input0));
+            break;
+        case 'email':
+            validateEmail();
+        default:
+            console.log("validateWithKeyWord() :>> No proper Key is send");
+    }
+}
 // #endregion Input Form
 /********************************************/
 /********************************************/
-
-
