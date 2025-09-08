14773436e2e2cb7850abff9d267d18050edf5b04
mahmoudRiad22 <mahmoudriad027@gmail.com>
2025-09-09 01:05:30 +0300

    removed config.js nad now testdeployment #8

--- week13/JsAssignment5-weatherApp/config.js
+++
@@ -1,4 +0,0 @@
-export const API_KEY = '${{ secrets.WEATHER_API_KEY }}';
-const live_Review = "https://mahmoudriad22.github.io/RouteAcademy_FrontEnd_with_Farouk_ZaaZaa/week13/JsAssignment5-weatherApp/index.html"
-
-const Code_Review = "[https://github.com/mahmoudRiad22/RouteAcademy_FrontEnd_with_Farouk_ZaaZaa/tree/main/week13/JsAssignment5-weatherApp/"
