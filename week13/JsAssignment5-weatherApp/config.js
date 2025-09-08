d2582b4a602347077c99f717a049d238ecde68d7
mahmoudRiad22 <mahmoudRiad22@users.noreply.github.com>
2025-09-08 22:07:22 +0000

    deploy: 14773436e2e2cb7850abff9d267d18050edf5b04

--- week13/JsAssignment5-weatherApp/config.js
+++ week13/JsAssignment5-weatherApp/config.js
@@ -1,4 +1,1 @@
-export const API_KEY = '${{ secrets.WEATHER_API_KEY }}';
-const live_Review = "https://mahmoudriad22.github.io/RouteAcademy_FrontEnd_with_Farouk_ZaaZaa/week13/JsAssignment5-weatherApp/index.html"
-
-const Code_Review = "[https://github.com/mahmoudRiad22/RouteAcademy_FrontEnd_with_Farouk_ZaaZaa/tree/main/week13/JsAssignment5-weatherApp/"
+export const API_KEY = 'e62895f9f3744dcea6d172527241907';
