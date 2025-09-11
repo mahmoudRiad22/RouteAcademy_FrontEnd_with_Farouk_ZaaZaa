1f3ab5c14e8385de8e5ad9bb0a4694b48b13c4eb
mahmoudRiad22 <mahmoudriad027@gmail.com>
2025-09-12 02:31:30 +0300

    added the email validation to the other pages

--- week13/JsAssignment5-weatherApp/JS/news.js
+++ week13/JsAssignment5-weatherApp/JS/news.js
@@ -10,3 +10,52 @@ const urlNewsByCity =
     `&pageSize=20`;
 
 console.log("urlNewsByCity :>> ", urlNewsByCity);
+
+
+
+const emailBar = document.getElementById("emailInput");
+emailBar.addEventListener("input", function () {
+    const emailLabel = document.getElementById("emailLabel");
+    if (emailBar.value) {
+        emailLabel.classList.add("d-none");
+    } else {
+        emailLabel.classList.remove("d-none");
+    }
+});
+
+const subscribeBtn = document.getElementById("subscribeBtn");
+subscribeBtn.addEventListener("click", () => {
+    const toastLiveExample = document.getElementById("liveToast");
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+
+    if (emailValidation(emailBar.value)) {
+        document.getElementById("toastBodyParent").classList.remove("my-bg-danger");
+        document.getElementById("toastBody").classList.remove("text-danger");
+
+        document.getElementById("toastBodyParent").classList.add("my-bg-success");
+        document.getElementById("toastBody").classList.add("my-text-sucess");
+        document.getElementById("toastBody").innerHTML = "Success!!";
+
+        toastBootstrap.show();
+        setTimeout(() => {
+            toastBootstrap.hide();
+        }, 1500);
+    } else {
+        document.getElementById("toastBodyParent").classList.remove("my-bg-success");
+        document.getElementById("toastBody").classList.remove("text-success");
+
+        document.getElementById("toastBodyParent").classList.add("my-bg-danger");
+        document.getElementById("toastBody").classList.add("text-danger");
+        document.getElementById("toastBody").innerHTML = "Invalid Email!!";
+
+        toastBootstrap.show();
+        setTimeout(() => {
+            toastBootstrap.hide();
+        }, 1500);
+    }
+});
+
+function emailValidation(input) {
+    const emailRegEx = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegEx.test(input);
+}
