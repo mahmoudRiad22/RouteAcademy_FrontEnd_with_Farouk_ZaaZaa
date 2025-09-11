91b14acadc574618ba3c24a9b0bbbc0b80470b61
mahmoudRiad22 <mahmoudRiad22@users.noreply.github.com>
2025-09-11 21:35:48 +0000

    deploy: 4c07447b26e0f410c3abff703177df6135063cd0

--- week13/JsAssignment5-weatherApp/config.js
+++ week13/JsAssignment5-weatherApp/config.js
@@ -1,5 +1,8 @@
-export const API_KEY = "e62895f9f3744dcea6d172527241907";
-// export const API_KEY = '${{ secrets.WEATHER_API_KEY }}';
+export const API_KEY = "085c8be0d7834559b81192425251109";
+// export const API_KEY = "65667bda3b254fa483e145229250809";// wagdy api for a trial pro-plan
+export const NEWS_API = "7756b586c82f446ba6336aee7d6d17ac"; // api for News
+export const UNSPLASH_ACCESS_KEY = "b3uKKQ87dRDWc6kfrNOCc3-F6ybQLlLyOwaXEzM2lTw"; // Key for Photos
+
 const live_Review =
     "https://mahmoudriad22.github.io/RouteAcademy_FrontEnd_with_Farouk_ZaaZaa/week13/JsAssignment5-weatherApp/index.html";
 
