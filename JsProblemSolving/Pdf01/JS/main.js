de08a9088aaf06167e3f73c18ecea4147b05219b
mahmoudRiad22 <mahmoudRiad22@users.noreply.github.com>
2025-09-13 21:22:46 +0000

    deploy: a72051b2e91ab0e84cc224b211a5afa313a804f6

--- JsProblemSolving/Pdf01/JS/main.js
+++ JsProblemSolving/Pdf01/JS/main.js
@@ -1,5 +1,58 @@
 function init(){
     return "Hello";
 }
+// const output = document.getElementById("output");
+// const clear = document.getElementById("clear");
+// clear.addEventListener("click",()=>{
+//     output.innerHTML = ''
+// })
+/********************************************/
+/********************************************/
+// #region Basic I/O and conditions
+console.clear();
 
-module.exports = {init}; 
+/////////////////////////
+// Problem 01:
+// document.getElementById("problem01Btn").addEventListener("click",()=>{
+//     let y = document.getElementById("problem01Input");
+//     problem01(y.value)
+// })
+function problem01(x){
+    const regex = /^\w+$/
+    
+    if ( !x)
+        return "error";
+    if (!regex.test(x))
+        return 'error'
+    if (x==0)
+        return 'error'
+    if (Number(x))
+        return 'error'
+    // if (regex.test(x))
+    //     return `Hello, ${x}! Welcome to the program`
+    let out = `Hello, ${x}! Welcome to the program`;
+    // output.innerHTML = out;
+    return out;
+}
+/////////////////////////
+// problem 02:
+// document.getElementById("problem02Btn").addEventListener("click",()=>{
+//     let y = document.getElementById("problem02Input");
+//     problem02(y.value)
+// })
+function problem02(x){
+
+    let out = `Hello, ${x}! Welcome to the program`;
+    // output.innerHTML = out;
+    return out;
+}
+/////////////////////////
+
+// #endregion Basic I/O and conditions
+/********************************************/
+/********************************************/
+module.exports = {
+    init,
+    problem01,
+    problem02,
+}; 
--- JsProblemSolving/Pdf01/tests/main.test.js
+++ JsProblemSolving/Pdf01/tests/main.test.js
@@ -1,6 +1,29 @@
+/********************************************/
+/********************************************/
+// #region init
+console.clear();
+
 const main = require("../JS/main.js");
 
 test('test for init', ()=>{
 
-expect(main.init()).toBe("Hell");
+expect(main.init()).toBe("Hello");
+})
+
+// #endregion init
+/********************************************/
+/********************************************/
+
+test('test for problem01', ()=>{
+
+expect(main.problem01("mahmoud")).toBe(`Hello, mahmoud! Welcome to the program`);
+expect(main.problem01("mahmo-ud")).toBe(`Hello, mahmo-ud! Welcome to the program`);
+expect(main.problem01("")).toBe(`error`);
+expect(main.problem01(null)).toBe(`error`);
+expect(main.problem01(undefined)).toBe(`error`);
+expect(main.problem01(" ")).toBe(`error`);
+expect(main.problem01(0)).toBe(`error`);
+expect(main.problem01("0")).toBe(`error`);
+expect(main.problem01("1221")).toBe(`error`);
+
 })
