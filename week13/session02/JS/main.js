54651ca3df31e33352f117e6227ac53ed42b9109
mahmoudRiad22 <mahmoudriad027@gmail.com>
2025-09-15 12:11:19 +0300

    week13 session02: string manibulation and "use strict"

--- week13/session02/JS/main.js
+++ week13/session02/JS/main.js
@@ -1,95 +1,100 @@
-function f1() {
-    return new Promise(function (resolved, rejected) {
-        console.log("function 1");
-        let error = 0;
-        if (!error) resolved();
-        else rejected();
-    });
-}
+/********************************************/
+/********************************************/
+// #region recape or whatever it is
+console.clear();
 
-function f2() {
-    return new Promise(function (resolved, rejected) {
-        console.log("function 2");
-        let error = 0;
-        if (!error) resolved();
-        else rejected();
-    });
-}
+function recape() {
+    function f1() {
+        return new Promise(function (resolved, rejected) {
+            console.log("function 1");
+            let error = 0;
+            if (!error) resolved();
+            else rejected();
+        });
+    }
 
-function f3() {
-    return new Promise(function (resolved, rejected) {
-        console.log("function 3");
-        let error = 0;
-        if (!error) resolved();
-        else rejected();
-    });
-}
+    function f2() {
+        return new Promise(function (resolved, rejected) {
+            console.log("function 2");
+            let error = 0;
+            if (!error) resolved();
+            else rejected();
+        });
+    }
 
-function f4() {
-    return new Promise(function (resolved, rejected) {
-        console.log("function 4");
-        let error = 0;
-        if (!error) resolved();
-        else rejected();
-    });
-}
+    function f3() {
+        return new Promise(function (resolved, rejected) {
+            console.log("function 3");
+            let error = 0;
+            if (!error) resolved();
+            else rejected();
+        });
+    }
 
-function f5() {
-    return new Promise(function (resolved, rejected) {
-        console.log("function 5");
-        let error = 0;
-        if (!error) resolved();
-        else rejected();
-    });
-}
+    function f4() {
+        return new Promise(function (resolved, rejected) {
+            console.log("function 4");
+            let error = 0;
+            if (!error) resolved();
+            else rejected();
+        });
+    }
 
-///// call back hell
+    function f5() {
+        return new Promise(function (resolved, rejected) {
+            console.log("function 5");
+            let error = 0;
+            if (!error) resolved();
+            else rejected();
+        });
+    }
 
-/// promise && promise status
+    ///// call back hell
 
-// promise status:
-// 1- pending
-// 2- settled: ===>  fulfilled (success, resolved)  or fail (rejected)
+    /// promise && promise status
 
-//  create a promise:
-//  1- build a promise as a constractor
-//  2- use it;
+    // promise status:
+    // 1- pending
+    // 2- settled: ===>  fulfilled (success, resolved)  or fail (rejected)
 
-// make the function "return a promise" to be used by the "then" and "catch"
-// const p1 = new Promise(
-//     function (resolved, rejected){
-//         f1();
-//     }
-// )
+    //  create a promise:
+    //  1- build a promise as a constractor
+    //  2- use it;
 
-// p1.then(f2).catch(f3)
+    // make the function "return a promise" to be used by the "then" and "catch"
+    // const p1 = new Promise(
+    //     function (resolved, rejected){
+    //         f1();
+    //     }
+    // )
 
-async function steps() {
-    await f1();
-    await f2();
-    await f3();
-    await f4();
-    await f5();
-    console.log("Finished");
-}
-steps();
+    // p1.then(f2).catch(f3)
 
-let recipes = []
-const Box = document.getElementById('box');
-async function getMeals(meal){
-    const response = await fetch(`https://forkify-api.herokuapp.com/api/search?q=${meal}`);
-    const data = await response.json();
-    console.log('data :>> ', data);
-    recipes = data.recipes
-    displayMeals()
+    async function steps() {
+        await f1();
+        await f2();
+        await f3();
+        await f4();
+        await f5();
+        console.log("Finished");
+    }
+    // steps();
 
-}   
+    let recipes = [];
+    const Box = document.getElementById("box");
+    async function getMeals(meal) {
+        const response = await fetch(`https://forkify-api.herokuapp.com/api/search?q=${meal}`);
+        const data = await response.json();
+        console.log("data :>> ", data);
+        recipes = data.recipes;
+        displayMeals();
+    }
 
-function displayMeals(){
-    let box = '';
+    function displayMeals() {
+        let box = "";
 
-    for (let i =0; i< recipes.length; i++){
-        box += `
+        for (let i = 0; i < recipes.length; i++) {
+            box += `
                         <div class="card-item col-md-2 col-lg-4 col-xl-3 col-xxl-2">
                             <div class="inner">
                                 <img class="w-100 d-block" src="${recipes[i].image_url}" alt="${recipes[i].image_url}"/>
@@ -102,8 +107,93 @@ function displayMeals(){
                             </div>
                         </div>
 
-        `
+        `;
+        }
+        Box.innerHTML = box;
     }
-    Box.innerHTML = box;
+    // getMeals('pizza')
 }
-getMeals('pizza')
+
+// recape()
+
+// #endregion recape or whatever it is
+/********************************************/
+/********************************************/
+
+/********************************************/
+/********************************************/
+// #region string manibulation
+function stringManibulation() {
+    console.clear();
+    let str = "          ,this, is, the, first, string, to, use, for, manibulation,   z";
+
+    // to uppercase
+    // to lowercase
+    // chatAT()
+    // includes
+    // split
+    // replace
+    // trim
+
+    console.log("str.toUpperCase() :>> ", str.toUpperCase());
+    console.log("str.toLowerCase() :>> ", str.toLowerCase());
+    console.log("str.charAt(15) :>> ", str.charAt(15));
+    console.log('str.includes("i") :>> ', str.includes());
+    console.log("str.split(/ b/) :>> ", str.split(/\b/));
+    console.log("str.split(/s/) :>> ", str.split(/\s/));
+    console.log("str.split(/,/) :>> ", str.split(/,/));
+    console.log("str.split().join() :>> ", str.split(/\b/).join(""));
+    console.log("str.trim() :>> ", str.trim().split(/,/));
+    console.log(
+        "str.trimStart().trimEnd().split(/,/) :>> ",
+        str.trimStart().trimEnd().split(/\x2c/)
+    );
+}
+// stringManibulation();
+// #endregion string manibulation
+/********************************************/
+/********************************************/
+
+/********************************************/
+/********************************************/
+// #region  Slobby VS strict mode
+
+// in future frameworks the strict mode is on by defualt and in classes too
+
+function slobbyVsStrictMode() {
+    console.clear();
+    ("use strict");
+    // if no decleration is found the variable is declared Golbal by default
+    // fixed in "use strict"
+    function test() {
+        const x = 200;
+    }
+    test();
+    let x = 4000;
+    console.log("x :>> ", x);
+
+    // dublicated parameters with no initialization override each other by sequence.
+    // fixed in "use strict"
+    function test2(x, y, z, m) {
+        console.log(y);
+    }
+
+    test2(30, 40, 50, 1000);
+
+    // the keyword "arguments" can be used for declerations of variables
+    // fixed in "use strict"
+    function test3(x, y) {
+        console.log(arguments);
+    }
+    test3(40, 50);
+    function argumentss() {
+        console.log("WTF");
+    }
+    argumentss();
+    // let arguments = 40;
+    console.log(arguments);
+}
+// slobbyVsStrictMode();
+// #endregion Slobby VS strict mode
+/********************************************/
+/********************************************/
